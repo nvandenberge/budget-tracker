e1296a0aeea1f0b6d7881e1e5b7052df7475b921
Nick Vandenberge <nvandenberge14@gmail.com>
2021-04-02 15:37:23 -0400

    updated service-worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,3 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -10,32 +7,33 @@ const FILES_TO_CACHE = [
   "/icons/icon-512x512.png",
 ];
 
-// The install handler for service worker
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
 self.addEventListener("install", event => {
-  console.log('INSTALL handler ran from service-worker')
   event.waitUntil(
     caches
-      .open(CACHE_NAME)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
+      .open(STATIC_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
 
-// The activate handler takes care of cleaning up old caches
-self.addEventListener("activate", (event) => {
-  console.log('ACTIVATE handler ran from service-worker')
-  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", event => {
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
   event.waitUntil(
     caches
       .keys()
-      .then((cacheNames) => {
+      .then(cacheNames => {
+        // return array of cache names that are old to delete
         return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
+          cacheName => !currentCaches.includes(cacheName)
         );
       })
-      .then((cachesToDelete) => {
+      .then(cachesToDelete => {
         return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
+          cachesToDelete.map(cacheToDelete => {
             return caches.delete(cacheToDelete);
           })
         );
@@ -44,24 +42,47 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-// Fetch handler to log requests
-self.addEventListener("fetch", (event) => {
-  console.log('FETCH handler ran from service-worker')
-  if (event.request.url.includes("/api/")) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+self.addEventListener("fetch", event => {
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
 
-        return caches.open(DATA_CACHE_NAME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/")) {
+    // make network request and fallback to cache if network request fails (offline)
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
       })
     );
+    return;
   }
+
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
 });
